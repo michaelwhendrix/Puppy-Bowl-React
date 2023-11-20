263a0b8e614594e476e950e353517c4036d0bc9f
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-20 13:40:44 -0600

    added fetch to get player list to allPyppies conponent

--- src/components/AllPuppies.jsx
+++ src/components/AllPuppies.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
 
 
 const AllPuppies = () => {
+    const [allPuppiesRoster, setAllPuppiesRoster] = useState([])
+
+    useEffect(()=> {
+        const getAllPuppies = async () => {
+            const response = await fetch (`https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players
+            `)
+            const responseJson = await response.json()
+            setAllPuppiesRoster(responseJson.data.players)
+            console.log(allPuppiesRoster)
+
+        }
+        getAllPuppies()
+    },[])
     return (
         <>
-          <h1>Puppy Bowl Roster</h1>  
+          {allPuppiesRoster && allPuppiesRoster.map((singlePuppy) => {
+            return <div key={singlePuppy.id}> 
+                        <p>{singlePuppy.name}</p>    
+                    </div>
+          })}  
         </>
 
     )
