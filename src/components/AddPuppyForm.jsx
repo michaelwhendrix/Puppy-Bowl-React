7e2831080c060a5ba1f1ba5adf79529191f0794c
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-21 08:04:07 -0600

    added defaults in form to prevent errors

--- src/components/AddPuppyForm.jsx
+++ src/components/AddPuppyForm.jsx
@@ -2,11 +2,11 @@ import { useState } from "react"
 import AddPuppy from "./AddPuppy"
 
 const AddPuppyForm = () => {
-const [newPuppyName, setNewPuppyName] =useState('')
-const [newPuppyBreed, setNewPuppyBreed] =useState('')
-const [newPuppyStatus, setNewPuppyStatus] =useState('')
-const [newPuppyImageUrl, setNewPuppyImageUrl] =useState('')
-const [newPuppyTeamId, setNewPuppyTeamId] =useState(null)
+const [newPuppyName, setNewPuppyName] =useState('Rufus')
+const [newPuppyBreed, setNewPuppyBreed] =useState('Cocker Spaniel')
+const [newPuppyStatus, setNewPuppyStatus] =useState('bench')
+const [newPuppyImageUrl, setNewPuppyImageUrl] =useState('https://r.ddmcdn.com/w_1012/s_f/o_1/cx_0/cy_0/cw_1012/ch_1518/APL/uploads/2019/12/Kenny-PBXVI.jpg')
+const [newPuppyTeamId, setNewPuppyTeamId] =useState(1)
 
     return (
         <>
